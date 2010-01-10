971a8ada909dad9f66d74be05fe0e7a015295459
Mark Wunsch <mark@markwunsch.com>
2010-01-10 12:59:09 -0500

    Further defining of Property. Property is the property of a microformat.

--- lib/hmachine/property.rb
+++ lib/hmachine/property.rb
@@ -1,10 +1,32 @@
 module HMachine
   class Property
     
-    attr_reader :name
+    # A property should be able to find itself within a node
+    # and extract it's own contents.
+    # It can possess other properties.
     
-    def initialize(name)
+    attr_reader :name, :property_of
+    
+    def initialize(name, property_of = Microformat::Base)
       @name = self.class.normalize(name)
+      @property_of = Microformat.normalize(property_of)
+    end
+    
+    # Is this a property of microformat?
+    def property_of?(microformat)
+      Microformat.normalize(microformat) == property_of
+    end
+    
+    # Get/Set a function that defines how to find the Property in a node.
+    # The Search function should return a Nokogiri::XML::NodeSet.
+    def search(&block)
+      @search = block if block_given?
+      @search || lambda{ |node| node.css(".#{name}") }
+    end
+    
+    # Search for the Property in a node
+    def find_in(node)
+      search.call(node)
     end
     
     def self.normalize(name)
--- test/property_test.rb
+++ test/property_test.rb
@@ -1,12 +1,39 @@
 require File.join(File.dirname(__FILE__), 'test_helper')
 
-class PropertyTest < Test::Unit::TestCase
+class PropertyTest < Test::Unit::TestCase  
   should 'have a name' do
     test = HMachine::Property.new(:fn)
-    assert test.name == :fn
+    assert_equal :fn, test.name
   end
   
   should 'normalize names' do
-    assert HMachine::Property.normalize("Email") == :email
+    assert_equal :email, HMachine::Property.normalize("Email")
+  end
+  
+  should 'be a property of a microformat' do
+    test = HMachine::Property.new(:fn, :hcard)
+    assert_equal HMachine::Microformat::HCard, test.property_of
+  end
+  
+  should 'test to see if it is a property of a microformat' do
+    test = HMachine::Property.new(:fn)
+    assert !test.property_of?(:hcard), "Property is the property of #{test.property_of}"
+  end
+  
+  should 'have a default search method' do
+    test = HMachine::Property.new(:fn)
+    assert_respond_to test.search, :call
+  end
+  
+  should 'be able to redefine search function' do
+    test = HMachine::Property.new(:fn)
+    test.search { test.name }
+    assert_equal :fn, test.search.call
+  end
+  
+  should 'be able to find itself in a node' do
+    html = get_fixture('hcard/commercenet.html')
+    fn = HMachine::Property.new(:fn)
+    assert fn.find_in(Nokogiri::HTML.parse(html))[0]['class'].include?(fn.name.to_s), "Property #{fn.name.inspect} not found."
   end
 end
