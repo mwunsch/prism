e99e67fdeae190abea436ca05f1497c5a2efdef0
Mark Wunsch <mark@markwunsch.com>
2010-01-11 12:11:55 -0500

    Property inherits its search methods from the HMachine parser.

--- lib/hmachine/property.rb
+++ lib/hmachine/property.rb
@@ -1,79 +1,20 @@
-require 'hmachine/microformat'
-require 'hmachine/pattern'
+require 'hmachine'
 
 module HMachine
   class Property
+    include HMachine
     
-    # A property should be able to find itself within a node
-    # and extract it's own contents.
-    # It can possess other properties.
-    
-    # Searches for content using patterns.
-    
-    attr_reader :name, :property_of, :parsers
+    attr_reader :name, :property_of
     
     def initialize(name, property_of = Microformat::Base)
-      @name = self.class.normalize(name)
+      @name = HMachine.normalize(name)
       @property_of = Microformat.normalize(property_of)
+      @search = lambda {|node| node.css(".#{@name}")}
     end
     
     # Is this a property of microformat?
     def property_of?(microformat)
       Microformat.normalize(microformat) == property_of
-    end
-    
-    # Get/Set a function that defines how to find the Property in a node.
-    # The Search function should return a Nokogiri::XML::NodeSet.
-    def search(&block)
-      @search = block if block_given?
-      @search || lambda{ |node| node.css(".#{name}") }
-    end
-    
-    # Search for the Property in a node
-    def find_in(node)
-      search.call(node)
-    end
-    
-    # Is the property found in node?
-    def found_in?(node)
-      !find_in(node).empty?
-    end
-    
-    # Define the patterns used to extract contents from node
-    # Can be symbols that match to a Pattern module, or a lambda
-    # that returns the contents of a node
-    def extract_with(*extraction_patterns)
-      @parsers = []
-      extraction_patterns.each do |pattern|
-        parser = pattern.respond_to?(:call) ? pattern : Pattern.map(pattern).parser
-        @parsers << parser
-      end
-    end
-    
-    # Try each defined extraction pattern to get the content for the property
-    def extract_from(node)
-      raise "The property #{name.intern} can not be found in this node" unless found_in?(node.parent)
-      return node.content unless parsers 
-      content = nil
-      parsers.each {|parser| 
-        if parser.call(node)
-          content = parser.call(node)
-          break
-        end
-      }
-      content || node.content
-    end
-    
-    # Parse the node, finding the property, and extract the content from it
-    def parse(node)
-      if found_in?(node)
-        contents = find_in(node).collect {|element| extract_from(element) }
-        (contents.length == 1) ? contents.first : contents
-      end
-    end
-    
-    def self.normalize(name)
-      name.to_s.strip.downcase.intern
     end
     
   end
--- test/property_test.rb
+++ test/property_test.rb
@@ -1,15 +1,19 @@
 require File.join(File.dirname(__FILE__), 'test_helper')
 
-class PropertyTest < Test::Unit::TestCase  
+class PropertyTest < Test::Unit::TestCase
+  should 'include the HMachine general parser' do
+    assert HMachine::Property.include?(HMachine), "Property includes #{HMachine::Property.ancestors.inspect}"
+  end
+  
   should 'have a name' do
     test = HMachine::Property.new(:fn)
     assert_equal :fn, test.name
   end
   
   should 'normalize names' do
-    assert_equal :email, HMachine::Property.normalize("Email")
+    assert_equal :email, HMachine::Property.new("Email").name
   end
-  
+
   should 'be a property of a microformat' do
     test = HMachine::Property.new(:fn, :hcard)
     assert_equal HMachine::Microformat::HCard, test.property_of
@@ -20,67 +24,11 @@ class PropertyTest < Test::Unit::TestCase
     assert !test.property_of?(:hcard), "Property is the property of #{test.property_of}"
   end
   
-  should 'add parsers used to extract content' do
-    test = HMachine::Property.new(:fn)
-    parser = lambda{|node| node.content }
-    test.extract_with parser
-    assert test.parsers.include?(parser), "Parsers include: #{test.parsers.inspect}"
-  end
-  
-  should 'extract content from a node' do
-    html = get_fixture('hcard/commercenet.html')
-    fn = HMachine::Property.new(:fn)
-    node = fn.find_in(Nokogiri::HTML.parse(html))[0]
-    assert_equal "CommerceNet", fn.extract_from(node)
-  end
-  
-  should 'cycle through content parsers until one returns a value' do
+  should 'parse a document to find itself' do
     test = HMachine::Property.new(:fn)
     html = get_fixture('hcard/commercenet.html')
-    node = test.find_in(Nokogiri::HTML.parse(html))[0]
-    parser = (1..5).collect {|i| lambda{i} }
-    test.extract_with *parser
-    assert_equal 1, test.extract_from(node)
+    doc = Nokogiri.parse(html)
+    fn = doc.css('.fn').first.content
+    assert_equal fn, test.parse(doc)
   end
-  
-  should "default to node content if no parsers are able to find it" do
-    test = HMachine::Property.new(:fn)
-    html = get_fixture('hcard/commercenet.html')
-    node = test.find_in(Nokogiri::HTML.parse(html))[0]
-    parser = (1..5).collect {|i| lambda{nil} }
-    test.extract_with *parser
-    assert_equal node.content, test.extract_from(node)
-  end
-  
-  should 'have a default search method' do
-    test = HMachine::Property.new(:fn)
-    assert_respond_to test.search, :call
-  end
-  
-  should 'be able to redefine search function' do
-    test = HMachine::Property.new(:fn)
-    test.search { test.name }
-    assert_equal :fn, test.search.call
-  end
-  
-  should 'be able to find itself in a node' do
-    html = get_fixture('hcard/commercenet.html')
-    fn = HMachine::Property.new(:fn)
-    assert fn.find_in(Nokogiri::HTML.parse(html))[0]['class'].include?(fn.name.to_s), "Property #{fn.name.inspect} not found."
-  end
-  
-  should 'test to see if the property can be found in a node' do
-    html = get_fixture('hcard/commercenet.html')
-    fn = HMachine::Property.new(:fn)
-    node = Nokogiri::HTML.parse(html)
-    assert fn.found_in?(node), "The property #{fn.name.inspect} is not found"
-  end
-  
-  should 'parse a node and get the property' do
-    html = get_fixture('hcard/commercenet.html')
-    node = Nokogiri::HTML.parse(html)
-    fn = HMachine::Property.new(:fn)
-    assert_equal "CommerceNet", fn.parse(node)
-  end
-  
 end
