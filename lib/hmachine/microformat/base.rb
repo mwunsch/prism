5fdb5f3f6c28b4bbf30134b75e9fa39f50628b10
Mark Wunsch <mark@markwunsch.com>
2010-01-11 13:24:33 -0500

    Refactoring Base to use new HMachine Module parser.

--- lib/hmachine/microformat/base.rb
+++ lib/hmachine/microformat/base.rb
@@ -1,42 +1,35 @@
+require 'hmachine/property'
+
 module HMachine
   module Microformat
     class Base
-      
-      # Validates that the node is a microformat
-      def self.validate(node)
-        validator.call(node)
-      end
-      
-      # Define a function that tests to make sure a given node contains
-      # the microformat. Must return truthy
-      def self.validator(&block)
-        @validator = block if block_given?
-        @validator || lambda { |node| node['class'] == self.root }
-      end
-      
-      # Define a function that takes a Nokogiri document 
-      # and search it for the microformat
-      def self.search(&block)
-        @search = block if block_given?
-        @search || lambda { |doc| doc.css(".#{self.root}") }
-      end
-      
-      # Performs a search for the microformat in a given Nokogiri::XML::Node
-      # returns a Nokogiri::XML::NodeSet
-      def self.find_in(document)
-        search.call(document)
-      end
+      extend HMachine
       
       def self.root(class_name=nil)
         @root = class_name if class_name
         @root
       end
       
-      def self.wiki_url(url = nil)
-        @wiki_url ||= url
+      def self.wiki_url(url=nil)
+        @wiki_url = url if url
+        @wiki_url || 'http://microformats.org/wiki'
       end
       
-      def self.has_one(property)
+      # Overrides the HMachine search method with a similar method, only
+      # defaulting to searching for the root class name
+      def self.search(&block)
+        @search = block if block_given?
+        @search || lambda { |doc| doc.css(".#{root}") }
+      end
+      
+      # Overrides the HMachine validate method with similar, only
+      # defaulting to checking against the class of name of the node 
+      def self.validate(&block)
+        @validate = block if block_given?
+        @validate || lambda { |node| node['class'] == root }
+      end
+      
+      def self.has_one(*properties,&block)        
         # hCard has_one bday
       end
       
@@ -45,21 +38,20 @@ module HMachine
       end
       
       def self.requires(property)
-        @requirements ||= []
         # hCard requires fn
       end
       
-      def self.requirements
-        @requirements
-      end
+      # def self.requirements
+      #   @requirements
+      # end
       
       # Searches for the property in the microformat avoiding nested microformats
       # returns a Nokogiri::XML::NodeSet
-      def self.search_for(property,node)
-        search = self.find_in(node)
-        search.unlink unless (search == node)
-        node.css(".#{property}")
-      end
+      # def self.search_for(property, node)
+      #   search = self.find_in(node)
+      #   search.unlink unless (search == node)
+      #   node.css(".#{property}")
+      # end
       
       attr_reader :node
       
--- test/microformat/base_test.rb
+++ test/microformat/base_test.rb
@@ -3,83 +3,73 @@ require File.join(File.dirname(__FILE__), '..', 'test_helper')
 class BaseTest < Test::Unit::TestCase
   setup do
     @html = get_fixture('hcard/commercenet.html')
-    @node = Nokogiri::HTML.parse(@html).css('.'+HMachine::Microformat::HCard::ROOT_CLASS)[0]
-    @hcard = HMachine::Microformat::Base.new(@node)
+    @doc = Nokogiri::HTML.parse(@html)
+    # @node = Nokogiri::HTML.parse(@html).css('.'+HMachine::Microformat::HCard::ROOT_CLASS)[0]
+    # @hcard = HMachine::Microformat::Base.new(@node)
     @nested = '<div class="vcard"><span class="fn">Mark Wunsch</span> and <div class="vcard"><span class="fn">Somebody else</span></div></div>'
   end
   
   describe 'Class' do
-    setup do
-      @test_class = Class.new(@hcard.class)
+    should 'be a subclass of Base' do
+      test_class = Class.new(HMachine::Microformat::Base)
+      assert_equal HMachine::Microformat::Base, test_class.superclass
     end
     
-    test 'inherits from Base' do
-      assert @test_class.superclass == @hcard.class
+    should 'include the HMachine general parser' do
+      test_class = Class.new(HMachine::Microformat::Base)
+      assert_respond_to test_class, :extract_with
     end
     
-    test 'defines a wiki url' do
-      @test_class.wiki_url 'http://microformats.org/wiki'
-      assert @test_class.wiki_url == 'http://microformats.org/wiki'
+    should 'define a root class name' do
+      test_class = Class.new(HMachine::Microformat::Base)
+      test_class.root 'vcard'
+      assert_equal 'vcard', test_class.root
     end
     
-    test 'validator defaults to checking root class name against attribute' do
-      @test_class.root 'vcard'
-      assert @test_class.validator.call @node
+    should 'define a wiki url' do
+      test_class = Class.new(HMachine::Microformat::Base)
+      test_class.wiki_url 'http://microformats.org/wiki/hcard'
+      assert_equal 'http://microformats.org/wiki/hcard', test_class.wiki_url
     end
     
-    test 'can define a new validator' do
-      @test_class.validator { false }
-      assert !@test_class.validator.call
+    should 'have a default wiki url' do
+      test_class = Class.new(HMachine::Microformat::Base)
+      assert_equal 'http://microformats.org/wiki', test_class.wiki_url
     end
     
-    test 'validates against supplied validator' do
-      @test_class.validator { "win" }
-      assert @test_class.validate(@node) == "win"
+    should 'have a default search method' do
+      test_class = Class.new(HMachine::Microformat::Base)
+      test_class.root 'vcard'
+      assert_equal test_class.root, test_class.search.call(@doc).first['class']
     end
     
-    test 'defines how to search for the microformat' do
-      @test_class.search { true }
-      assert @test_class.search.call
-    end
-    
-    test 'performs a search against a document' do
-      @test_class.search {|doc| doc.css('.vcard') }
-      assert @test_class.find_in(Nokogiri::HTML.parse(@html)).respond_to? :length
-    end
-    
-    test 'defines a root class name' do
-      @test_class.root 'vcard'
-      assert @test_class.root == 'vcard'
-    end
-    
-    test 'searches for a property in a microformat' do
-      @test_class.root 'vcard'
-      node = @test_class.find_in(Nokogiri::HTML.parse(@nested))[0]
-      test = @hcard.class.new(node)
-      assert @test_class.search_for(:fn, node)[0].content == "Mark Wunsch"
-    end
-    
-    test 'does not search for properties within a nested microformat' do
-      @test_class.root 'vcard'
-      node = @test_class.find_in(Nokogiri::HTML.parse(@nested))[0]
-      test = @hcard.class.new(node)
-      assert @test_class.search_for(:fn, node).length == 1
-    end
-    
+    should 'have a default validate method' do
+      test_class = Class.new(HMachine::Microformat::Base)
+      test_class.root 'vcard'
+      node = @doc.css('.vcard').first
+      assert test_class.valid?(node)
+    end 
     
   end
   
   describe 'Instance' do
-    test 'possesses an html node' do
-      assert @hcard.node == @node
+    setup do
+      @node = @doc.css('.vcard').first
     end
     
-    test 'converts its node to an html representation' do
-      assert @hcard.to_s == @node.to_s
+    should 'possess an html node' do
+      test = HMachine::Microformat::Base.new(@node)
+      assert_equal @node, test.node
     end
     
-    test 'also has a convenience method to convert to html' do
-      assert @hcard.to_html == @node.to_html
+    should 'convert its node to an html representation' do
+      test = HMachine::Microformat::Base.new(@node)
+      assert_equal @node.to_s, test.to_s
+    end
+    
+    should 'have a convenience method to convert to html' do
+      test = HMachine::Microformat::Base.new(@node)
+      assert_equal @node.to_html, test.to_html
     end
   end
   
