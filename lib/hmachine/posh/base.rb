c93e0a2dc9ee89d214c645112bb8e767e458f6da
Mark Wunsch <mark@markwunsch.com>
2010-02-08 21:07:35 -0500

    POSH::Base defining; revisiting ideas from Microformat::Base. Microformat::Base will eventually inherit from POSH::Base.

--- lib/hmachine/posh/base.rb
+++ lib/hmachine/posh/base.rb
@@ -2,11 +2,19 @@ module HMachine
   module POSH
     class Base
       extend HMachine
-
+      
       # Instead of getting the contents of a node, this creates
       # a POSH format from the node
       def self.extract
         lambda{|node| self.new(node) }
+      end
+      
+      def self.properties
+        @properties ||= {}
+      end
+      
+      def self.[](key)
+        properties[key]
       end
 
       # Remove a format from a node if it is nested.
@@ -17,11 +25,82 @@ module HMachine
         node
       end
       
+      def self.add_property(property_name, block=nil)
+        property = Property.new(property_name, self)
+        block.call(property) if block
+        properties[property_name] = property
+        property
+      end
+      
+      def self.has_one(*property_names, &block)
+        property_names.collect do |name|
+          property = has_one!(name, block)
+          define_method property.name do 
+            self[property.name]
+          end
+          property
+        end
+      end
+      
+      def self.has_many(*property_names, &block)
+        property_names.collect do |name|
+          property = has_many!(name, block)
+          define_method property.name do 
+            self[property.name]
+          end
+          property
+        end
+      end
+      
+      def self.has_one!(property_name, function=nil, &block)
+        block = function if (block.nil? && function)
+        @has_one ||= []
+        property = add_property(property_name, block)
+        @has_one << property
+        property
+      end
+      
+      def self.has_many!(property_name, function=nil, &block)
+        block = function if (block.nil? && function)
+        @has_many ||= []
+        property = add_property(property_name, block)
+        @has_many << property
+        property
+      end
+      
       attr_reader :node
       
       def initialize(node)
         raise 'Uh OH' unless self.class.valid?(node)
         @node = node
+      end
+      
+      def [](property_key)
+        to_h[property_key]
+      end
+      
+      def to_h
+        @to_h ||= {}
+        has_one = self.class.instance_variable_get(:@has_one)
+        self.class.properties.each_pair do |key, property|
+          if property.found_in?(node)
+            if (has_one && has_one.include?(property))
+              value = property.parse_first(node)
+            else
+              value = property.parse(node)
+            end
+            @to_h[key] = value
+          end
+        end
+        @to_h
+      end
+      
+      def to_s
+        node.to_s
+      end
+      
+      def to_html
+        node.to_html
       end
 
     end
--- test/posh/base_test.rb
+++ test/posh/base_test.rb
@@ -7,21 +7,66 @@ class PoshTest < Test::Unit::TestCase
   end
   
   describe 'Inheritance' do
-    
-    should 'parse itself out of a document' do
-      test_class = Class.new(HMachine::POSH::Base)
-      property = 'fn'
-      test_class.search { |doc| doc.css(".#{property}") }
-      test_class.validate { |node| node['class'] && node['class'].split(' ').include?(property) }
-      assert_instance_of test_class, test_class.parse(@doc)
+    setup do
+      @test_class = Class.new(HMachine::POSH::Base)
+      property = 'vcard'
+      @test_class.search { |doc| doc.css(".#{property}") }
+      @test_class.validate { |node| node['class'] && node['class'].split(' ').include?(property) }
     end
     
+    should 'parse itself out of a document' do
+      assert_instance_of @test_class, @test_class.parse(@doc)
+    end
+    
+    should "add a property to its group of properties" do
+      property = @test_class.add_property(:fn)
+      assert_equal HMachine::Property, property.class
+      assert_equal property, @test_class.properties[:fn]
+    end
+    
+    should 'further refine a property with a block' do
+      property = @test_class.add_property(:fn, lambda{|p| p.subproperties :n } )
+      assert_equal property[:n], @test_class.properties[:fn][:n]
+    end
+    
+    should 'find a property' do
+      property = @test_class.add_property(:fn)
+      assert_equal property, @test_class[:fn]
+    end
+    
+    should 'remove nested nodes from a node' do
+      nested = '<div class="vcard"><span class="fn">Mark Wunsch</span> and <div class="vcard"><span class="fn">Somebody else</span></div></div>'
+      parsed = @test_class.find_in(Nokogiri::HTML.parse(nested)).first
+      assert_equal "Mark Wunsch and", @test_class.remove_nested(parsed).content.strip
+    end
+    
+    should 'have one property' do
+      property = @test_class.has_one!(:fn)
+      assert_equal 1, @test_class.instance_variable_get(:@has_one).length
+    end
+    
+    should 'has many properties' do
+      property = @test_class.has_many!(:tel)
+      assert_equal property, @test_class.instance_variable_get(:@has_many).first
+    end
+    
+    should 'have one property and define an instance method' do
+      property = @test_class.has_one :fn
+      assert_equal property.first, @test_class.instance_variable_get(:@has_one).first
+      assert_respond_to @test_class.parse(@doc), :fn
+      assert_equal property.first.parse_first(@doc), @test_class.parse(@doc).fn
+    end
+    
+    should 'have many of a type of properties and define an instance method' do
+      property = @test_class.has_many :tel
+      assert_respond_to @test_class.parse(@doc), :tel
+    end
   end
   
   describe 'Instance' do
     setup do
       @klass = Class.new(HMachine::POSH::Base)
-      property = 'fn'
+      property = 'vcard'
       @klass.search { |doc| doc.css(".#{property}") }
       @klass.validate { |node| node['class'] && node['class'].split(' ').include?(property) }
       @node = @klass.find_in(@doc).first
@@ -29,6 +74,33 @@ class PoshTest < Test::Unit::TestCase
     
     should 'have a node' do
       assert_equal @node, @klass.new(@node).node
+    end
+    
+    should 'lookup a property value' do
+      fn = @klass.has_one :fn
+      vcard = @klass.new(@node)
+      assert_equal vcard.to_h[:fn], vcard[:fn]
+      assert_equal fn.first.parse_first(@node), vcard[:fn]
+    end
+    
+    should 'convert to a hash' do
+      fn = @klass.has_one :fn
+      tel = @klass.has_many :tel
+      @klass.has_many :foobar
+      vcard = @klass.new(@node)
+      assert vcard.to_h.has_key?(:fn)
+      assert vcard.to_h.has_key?(:tel)
+      assert !vcard.to_h.has_key?(:foobar)
+    end
+    
+    should 'have a DSL for defining properties' do
+      @klass.has_one :fn
+      @klass.has_many :tel
+      @klass.has_many! :foobar
+      vcard = @klass.new(@node)
+      assert_respond_to vcard, :fn
+      assert_respond_to vcard, :tel
+      assert !vcard.respond_to?(:foobar)
     end
     
   end
