468ea839051b209c8efd6045e4129b4bddd186fb
Mark Wunsch <mark@markwunsch.com>
2010-02-10 13:15:48 -0500

    Better handling for multiple nested DLs

--- lib/hmachine/posh/definition_list.rb
+++ lib/hmachine/posh/definition_list.rb
@@ -11,8 +11,18 @@ module HMachine
         terms = dl.children.select {|dt| dt.elem? && dt.node_name.eql?('dt') }
         terms.each do |term|
           definition = term.next_element if term.next_element.node_name.eql?('dd')
-          nested_dls = definition.children.select {|dd| dd.elem? && dd.node_name.eql?('dl') }          
-          value = nested_dls.empty? ? definition.content.strip : build_dictionary(nested_dls.first)
+          nested_dls = definition.children.select {|dd| dd.elem? && dd.node_name.eql?('dl') } 
+          if nested_dls.empty?
+            value = definition.content.strip
+          elsif nested_dls.length.eql?(1)
+            value = build_dictionary(nested_dls.first)
+          else
+            value = {}
+            nested_dls.each do |dict| 
+              value.merge! build_dictionary(dict) 
+            end
+          end
+          # value = nested_dls.empty? ? definition.content.strip : build_dictionary(nested_dls.first)
           dictionary[term.content.strip.intern] = value
         end
         dictionary
