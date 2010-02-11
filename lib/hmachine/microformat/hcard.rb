92b97509ceeed3e1af2c362104c8a740f3331031
Mark Wunsch <mark@markwunsch.com>
2010-02-11 15:17:06 -0500

    Beginning hCard tests

--- lib/hmachine/microformat/hcard.rb
+++ lib/hmachine/microformat/hcard.rb
@@ -1,44 +1,50 @@
 module HMachine
   module Microformat
-    class HCard < Base
-                  
-      root 'vcard'    
-      wiki_url "http://microformats.org/wiki/hcard"
-      invalid_msg "Not a valid hCard."
+    class HCard < POSH::Base
+      WIKI_URL = "http://microformats.org/wiki/hcard"
+      XMDP = 'http://microformats.org/profile/hcard'
       
-      has_one :fn
+      search {|doc| doc.css('.vcard') }
       
-      has_one :n do |n|
-        # n.subproperties :family_name, :given_name, :additional_name, 
-        #                 :honorific_prefix, :honorific_suffix
-      end
-
-      has_many :adr do |adr|
-        # adr.subproperties :post_office_box, :extended_address, :street_address, :locality,
-        #                   :region, :postal_code, :country_name, :type, :value
-      end
-
-      has_many :agent, :category, :key, :label, :logo, :mailer, :nickname,
-               :note, :photo, :role, :sound, :title, :url
-               
-      has_many :email do |email|
-        # email.subproperties :type, :value
+      validate {|vcard| vcard.matches?('.vcard') }
+      
+      has_one :fn do |fn| 
+        fn.extract :valueclass
       end
       
-      has_many :tel do |tel|
-        # tel.subproperties :type, :value
-      end
-      
-      has_many :geo do |geo|
-        # geo.subproperties :latitude, :longitude
-      end
-      
-      has_many :org do |org|
-        # org.subproperties :organization_name, :organization_unit
-      end
-      
-      has_one 'bday', 'tz', 'geo', 'sort-string', 'uid', 'rev'
-      # Causing a Segmentation Fault => :class
+      # has_one :fn
+      # 
+      # has_one :n do |n|
+      #   n.subproperties :family_name, :given_name, :additional_name, 
+      #                   :honorific_prefix, :honorific_suffix
+      # end
+      # 
+      # has_many :adr do |adr|
+      #   adr.subproperties :post_office_box, :extended_address, :street_address, :locality,
+      #                     :region, :postal_code, :country_name, :type, :value
+      # end
+      # 
+      # has_many :agent, :category, :key, :label, :logo, :mailer, :nickname,
+      #          :note, :photo, :role, :sound, :title, :url
+      #          
+      # has_many :email do |email|
+      #   email.subproperties :type, :value
+      # end
+      # 
+      # has_many :tel do |tel|
+      #   tel.subproperties :type, :value
+      # end
+      # 
+      # has_many :geo do |geo|
+      #   geo.subproperties :latitude, :longitude
+      # end
+      # 
+      # has_many :org do |org|
+      #   org.subproperties :organization_name, :organization_unit
+      # end
+      # 
+      # has_one :bday, :tz, :geo, :sort_string, :uid, :rev, :_class 
+      # # Calling this _class, because class causes a seg fault
             
     end
   end
--- test/microformat/hcard_test.rb
+++ test/microformat/hcard_test.rb
@@ -4,53 +4,18 @@ class HCardTest < Test::Unit::TestCase
   setup do
     @html = get_fixture('hcard/commercenet.html')
     @doc = Nokogiri::HTML.parse(@html)
+    @klass = HMachine::Microformat::HCard
   end
   
-  describe 'Class' do
-    should 'inherit from Base' do
-      assert_equal HMachine::Microformat::Base, HMachine::Microformat::HCard.superclass
-    end
-    
-    should 'have a url to a wiki page' do
-      assert_equal "http://microformats.org/wiki/hcard", HMachine::Microformat::HCard.wiki_url
-    end
-    
-    should 'have a root class to search for' do
-      assert_equal "vcard", HMachine::Microformat::HCard.root
-    end
-    
-    should 'find itself in a document' do
-      assert_equal 1, HMachine::Microformat::HCard.find_in(@doc).length
-    end
+  should 'parse itself from a document' do
+    hcard = @klass.parse(@doc)
+    assert_respond_to hcard, :fn
+    assert hcard.node['class'].include?('vcard')
   end
   
-  describe 'Instance' do
-    setup do
-      @node = HMachine::Microformat::HCard.find_in(@doc).first
-    end
-    
-    should 'validate the node' do
-      assert_nothing_raised do
-        HMachine::Microformat::HCard.new(@node)
-      end      
-    end
-    
-    should 'have one fn' do
-      test = HMachine::Microformat::HCard.new(@node)
-      assert_respond_to test, :fn
-    end
-    
-    should 'have one n' do
-      test = HMachine::Microformat::HCard.new(@node)
-      assert_respond_to test, :n
-    end
-  end
-  
-  describe 'Parsing' do
-    setup do
-      @node = HMachine::Microformat::HCard.find_in(@doc).first
-      @hcard = HMachine::Microformat::HCard.new(@node)
-    end
+  should 'have an fn' do
+    hcard = @klass.parse(@doc)
+    assert_equal 'CommerceNet', hcard.fn
   end
 
 end
