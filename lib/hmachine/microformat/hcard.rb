4c852a37563ac63b115378b5f07f8f38a6d9ae08
Mark Wunsch <mark@markwunsch.com>
2010-02-16 18:11:48 -0500

    Cleaning up HCard. Breaking off for Adr microformat, TypeValue extraction

--- lib/hmachine/microformat/hcard.rb
+++ lib/hmachine/microformat/hcard.rb
@@ -16,8 +16,7 @@ module HMachine
                :title, :url
       
       has_one :geo do |geo|
-        geo.has_one :latitude
-        geo.has_one :longitude
+        geo.has_one :latitude,  :longitude
         
         geo.extract do |node|
           if geo[:latitude].found_in?(node) && geo[:longitude].found_in?(node)
@@ -80,40 +79,6 @@ module HMachine
         fn == org
       end
       alias company? organization?
-      
-      # has_one :fn
-      # 
-      # has_one :n do |n|
-      #   n.subproperties :family_name, :given_name, :additional_name, 
-      #                   :honorific_prefix, :honorific_suffix
-      # end
-      # 
-      # has_many :adr do |adr|
-      #   adr.subproperties :post_office_box, :extended_address, :street_address, :locality,
-      #                     :region, :postal_code, :country_name, :type, :value
-      # end
-      # 
-      # has_many :agent, :category, :key, :label, :logo, :mailer, :nickname,
-      #          :note, :photo, :role, :sound, :title, :url
-      #          
-      # has_many :email do |email|
-      #   email.subproperties :type, :value
-      # end
-      # 
-      # has_many :tel do |tel|
-      #   tel.subproperties :type, :value
-      # end
-      # 
-      # has_many :geo do |geo|
-      #   geo.subproperties :latitude, :longitude
-      # end
-      # 
-      # has_many :org do |org|
-      #   org.subproperties :organization_name, :organization_unit
-      # end
-      # 
-      # has_one :bday, :tz, :geo, :sort_string, :uid, :rev, :_class 
-      # # Calling this _class, because class causes a seg fault
             
     end
   end
