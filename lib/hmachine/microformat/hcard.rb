6cbd84a15b030c2ffe3490547e6c52d9a3bcb3bd
Mark Wunsch <mark@markwunsch.com>
2010-02-16 14:06:33 -0500

    hCard tests and test fixtures

--- lib/hmachine/microformat/hcard.rb
+++ lib/hmachine/microformat/hcard.rb
@@ -8,9 +8,55 @@ module HMachine
       
       validate {|vcard| vcard.matches?('.vcard') }
       
-      has_one :fn do |fn| 
-        fn.extract :valueclass
+      has_one :fn
+      
+      has_one :org do |org|
+        org.has_one :organization_name, :organization_unit
       end
+      
+      has_one :n do |n|
+        n.search do |doc|
+          found = doc.css('.n')
+          if !found.empty?
+            found
+          else
+            properties[:fn].find_in(doc)
+          end
+        end
+        
+        n.has_one :family_name, :given_name, :additional_name,
+                  :honorific_prefix, :honorific_suffix
+        
+        n.extract do |node|
+          if node.matches?('.n')
+            n.property_hash(node)
+          else
+            n_optimization(node)
+          end
+        end  
+      end
+      
+      # N Optimization from Sumo:
+      # http://www.danwebb.net/2007/2/9/sumo-a-generic-microformats-parser-for-javascript
+      # See: http://microformats.org/wiki/hcard#Implied_.22n.22_Optimization
+      def self.n_optimization(node)
+        fn = properties[:fn].parse_first(node.parent)
+        org = properties[:org].parse_first(node.parent)
+        if (fn != org)
+          if (fn =~ /^(\w+) (\w+)$/)
+            { :given_name => Regexp.last_match[1],
+              :family_name => Regexp.last_match[2] }
+          elsif (fn =~ /^(\w+), (\w+)\.?$/)
+            { :given_name => Regexp.last_match[2],
+              :family_name => Regexp.last_match[1] }
+          end
+        end
+      end
+      
+      def organization?
+        fn == org
+      end
+      alias company? organization?
       
       # has_one :fn
       # 
--- test/microformat/hcard_test.rb
+++ test/microformat/hcard_test.rb
@@ -1,21 +1,24 @@
 require File.join(File.dirname(__FILE__), '..', 'test_helper')
 
 class HCardTest < Test::Unit::TestCase
-  setup do
-    @html = get_fixture('hcard/commercenet.html')
-    @doc = Nokogiri::HTML.parse(@html)
-    @klass = HMachine::Microformat::HCard
-  end
-  
-  should 'parse itself from a document' do
-    hcard = @klass.parse(@doc)
-    assert_respond_to hcard, :fn
-    assert hcard.node['class'].include?('vcard')
-  end
-  
-  should 'have an fn' do
-    hcard = @klass.parse(@doc)
-    assert_equal 'CommerceNet', hcard.fn
+  describe 'single occurence test' do
+    setup do
+      @html = get_fixture('test-fixture/hcard/hcard1.html')
+      @doc = Nokogiri::HTML.parse(@html).css('#uf').first
+      @klass = HMachine::Microformat::HCard
+    end
+    
+    test 'The fn (formatted name) is a singular value' do
+      vcard = @klass.parse_first(@doc)
+      assert_respond_to vcard, :fn
+      assert_equal 'John Doe', vcard.fn
+    end
+    
+    test 'The n (name) is a singular value' do
+      vcard = @klass.parse_first(@doc)
+      assert_respond_to vcard, :n
+      assert vcard.has_property?(:n)
+    end
   end
 
 end
