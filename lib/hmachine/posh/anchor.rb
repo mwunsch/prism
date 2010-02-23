844e3e5df2be9335519408f5e7fabaa766cf6771
Mark Wunsch <mark@markwunsch.com>
2010-02-23 14:57:20 -0500

    Updating anchor to reflect new Base

--- lib/hmachine/posh/anchor.rb
+++ lib/hmachine/posh/anchor.rb
@@ -2,36 +2,33 @@ module HMachine
   module POSH
     class Anchor < Base
       
-      search {|doc| doc.css('a') }
-      
-      validate {|node| node.elem? && (node.node_name == 'a') }
-      
-      has_one :url do |url|
-        url.search {|node| node }
-        url.extract {|node| node['href'] }
+      selector "a"
+            
+      has_one :url do
+        search {|node| node }
+        extract {|node| node['href'] }
       end
       alias href url
       
-      has_one :text do |text|
-        text.search {|node| node }
-        text.extract {|node| node.content.strip }
+      has_one :text do
+        search {|node| node }
       end
       alias content text
       
-      has_one :rel do |rel|
-        rel.search {|node| node }
-        rel.extract {|node| node['rel'].split(' ') }
+      has_one :rel do
+        search {|node| node }
+        extract {|node| node['rel'].split }
       end
       alias relationship rel
       
-      has_one :title do |title|
-        title.search {|node| node }
-        title.extract {|node| node['title'] }
+      has_one :title do
+        search {|node| node }
+        extract {|node| node['title'] }
       end
       
-      has_one :type do |type|
-        type.search {|node| node }
-        type.extract {|node| node['type'] }
+      has_one :type do
+        search {|node| node }
+        extract {|node| node['type'] }
       end
       
       def to_s
