436167bc7ea42aedf86a621a3eaa12f18cc99271
Mark Wunsch <mark@markwunsch.com>
2010-02-25 11:36:40 -0500

    DRYing up GEO

--- lib/prism/microformat/geo.rb
+++ lib/prism/microformat/geo.rb
@@ -6,32 +6,22 @@ module Prism
       
       name :geo
       
-      has_one :latitude do
+      has_one :latitude, :longitude do
         search do |geo| 
-          lat = geo.css(".#{name}")
-          !lat.empty? ? lat : geo
+          coord = geo.css(".#{name}")
+          !coord.empty? ? coord : geo
         end
         
         extract do |geo|
           if geo['class'] && geo['class'].split.include?("#{name}")
             Prism::Pattern::ValueClass.extract_from(geo)
           else
-            Prism::Pattern::ValueClass.extract_from(geo).split(';')[0]
-          end
-        end
-      end
-      
-      has_one :longitude do
-        search do |geo| 
-          long = geo.css(".#{name}")
-          !long.empty? ? long : geo
-        end
-        
-        extract do |geo|
-          if geo['class'] && geo['class'].split.include?("#{name}")
-            Prism::Pattern::ValueClass.extract_from(geo)
-          else
-            Prism::Pattern::ValueClass.extract_from(geo).split(';')[1]
+            coords = Prism::Pattern::ValueClass.extract_from(geo).split(';')
+            if name.eql?(:latitude)
+              coords[0]
+            elsif name.eql?(:longitude)
+              coords[1]
+            end
           end
         end
       end
