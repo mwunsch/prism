3ba6f2ef385e6a818eaf076eac2080c033986dbe
Mark Wunsch <mark@markwunsch.com>
2010-03-21 00:44:22 -0400

    ADR test cleanup

--- test/microformat/adr_test.rb
+++ test/microformat/adr_test.rb
@@ -17,22 +17,22 @@ class AdrTest < Test::Unit::TestCase
   
   should 'have a street address' do
     assert @adr.has_property?(:street_address)
-    assert_equal '665 3rd St.', @adr.street_address
+    assert_equal '665 3rd St.', @adr.street_address[0]
   end
   
   should 'have an extended address' do
     assert @adr.has_property?(:extended_address)
-    assert_equal 'Suite 207', @adr.extended_address
+    assert_equal 'Suite 207', @adr.extended_address[0]
   end
   
   should 'have a locality' do
     assert @adr.has_property?(:locality)
-    assert_equal 'San Francisco', @adr.locality
+    assert_equal 'San Francisco', @adr.locality[0]
   end
   
   should 'have a region' do
     assert @adr.has_property?(:region)
-    assert_equal 'CA', @adr.region
+    assert_equal 'CA', @adr.region[0]
   end
   
   should 'have a postal code' do
@@ -42,6 +42,6 @@ class AdrTest < Test::Unit::TestCase
   
   should 'have a country name' do
     assert @adr.has_property?(:country_name)
-    assert_equal 'U.S.A.', @adr.country_name
+    assert_equal 'U.S.A.', @adr.country_name[0]
   end
 end
